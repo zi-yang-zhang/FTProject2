87a114215dde3d9ec025e355a81f6815d2e5b3bc
Robert Zhang <ZiYang_Zhang@McAfee.com>
2015-04-11 17:04:19 -0400

    clean up

--- graphGenerator.py
+++
@@ -1,9 +0,0 @@
-import util
-from model.Vertex import Vertex
-
-def generate(dimension):
-	graph = []
-	for node in xrange(0, 2**dimension):
-		graph.append(Vertex(util.toBinary(node,dimension)))
-
-	return graph
